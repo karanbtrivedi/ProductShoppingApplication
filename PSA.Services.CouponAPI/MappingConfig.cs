61f8d8cf81e6f71382912a61062f6417addb89a5
karanbtrivedi <trivedikaran13@gmail.com>
2024-05-15 14:41:44 +0530

    First Phase : 00:00:00 - 01:46:00 (Date : 15/May/2024)

---
+++ PSA.Services.CouponAPI/MappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using PSA.Services.CouponAPI.Models;
+using PSA.Services.CouponAPI.Models.Dto;
+
+namespace PSA.Services.CouponAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<CouponDto, Coupon>();
+                config.CreateMap<Coupon, CouponDto>();
+            });
+            return mappingConfig;
+        }
+    }
+}
---
+++ PSA.Web/Utility/SD.cs
@@ -0,0 +1,20 @@
+﻿namespace PSA.Web.Utility
+{
+    public class SD
+    {
+        public static string CouponAPIBase { get; set; }
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+
+        public enum ContentType
+        {
+            Json,
+            MultipartFormData,
+        }
+    }
+}
